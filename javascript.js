397cb8e7fb31cc44bdf61734cd4f9013d6702b55
Libero Lance <mfc2294@gmail.com>
2022-11-20 10:26:15 -0500

    Make dynamic dimensions and add event listener to mouse hover

--- javascript.js
+++ javascript.js
@@ -1,12 +1,19 @@
 const container = document.querySelector('#grid-container');
-const row = 4;
-const column = 8;
+const rows = 8;
+const columns = 8;
 
-for(let i = 0; i < row; i++){
-	for(let j = 0; j < column; j++){
-		const square = document.createElement('div');
-		square.classList.add('square');
-		square.textContent = 'x';
-		container.appendChild(square);
-	} 
+for(let i = 0; i < (rows * columns); i++){
+	const square = document.createElement('div');
+	square.classList.add('square');
+	square.textContent = i + 1;
+	container.appendChild(square); 
 }
+//set correct nxn style for grid
+container.style.setProperty('grid-template-columns', `repeat(${columns}, auto)`);
+container.style.setProperty('grid-template-rows', `repeat(${rows}, auto)`);
+
+//add mouse over event
+container.addEventListener('mouseover', (e) => {
+  e.target.style.backgroundColor = 'yellow';
+});
+
